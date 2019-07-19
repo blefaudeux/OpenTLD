54893c53090d5a9cf5d2ace3cc78116699ee98c3
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2019-07-18 21:30:28 -0700

    build fix

--- src/tld/MedianFlowTracker.cpp
+++ src/tld/MedianFlowTracker.cpp
@@ -27,48 +27,55 @@
 #include "fbtrack.h"
 #include <cmath>
 
-namespace tld {
+namespace tld
+{
 
-    MedianFlowTracker::MedianFlowTracker() {
-        trackerBB = NULL;
+MedianFlowTracker::MedianFlowTracker()
+{
+    trackerBB = NULL;
+}
+
+MedianFlowTracker::~MedianFlowTracker()
+{
+    cleanPreviousData();
+}
+
+void MedianFlowTracker::cleanPreviousData()
+{
+    trackerBB.reset();
+}
+
+void MedianFlowTracker::track(Mat prevMat, Mat currMat, Rect &prevBB)
+{
+    if (prevBB.width <= 0 || prevBB.height <= 0)
+    {
+        return;
     }
 
-    MedianFlowTracker::~MedianFlowTracker() {
-        cleanPreviousData();
+    float bb_tracker[] = {static_cast<float>(prevBB.x), static_cast<float>(prevBB.y), static_cast<float>(prevBB.width + prevBB.x - 1), static_cast<float>(prevBB.height + prevBB.y - 1)};
+    float scale;
+
+    IplImage prevImg = prevMat;
+    IplImage currImg = currMat;
+
+    int success = fbtrack(&prevImg, &currImg, bb_tracker, bb_tracker, &scale);
+
+    //Extract subimage
+    float x, y, w, h;
+    x = floor(bb_tracker[0] + 0.5);
+    y = floor(bb_tracker[1] + 0.5);
+    w = floor(bb_tracker[2] - bb_tracker[0] + 1 + 0.5);
+    h = floor(bb_tracker[3] - bb_tracker[1] + 1 + 0.5);
+
+    //TODO: Introduce a check for a minimum size
+    if (!success || x < 0 || y < 0 || w <= 0 || h <= 0 || x + w > currMat.cols || y + h > currMat.rows || x != x || y != y || w != w || h != h)
+    { //x!=x is check for nan
+        //Leave it empty
     }
-
-    void MedianFlowTracker::cleanPreviousData() {
-        trackerBB.reset();
+    else
+    {
+        trackerBB.reset(new Rect(x, y, w, h));
     }
-
-    void MedianFlowTracker::track(Mat prevMat, Mat currMat, Rect & prevBB) {
-        if(prevBB.width <= 0 || prevBB.height <= 0) {
-            return;
-        }
-
-        float bb_tracker[] = {prevBB.x, prevBB.y, prevBB.width+prevBB.x-1, prevBB.height+prevBB.y-1};
-        float scale;
-
-        IplImage prevImg = prevMat;
-        IplImage currImg = currMat;
-
-        int success = fbtrack(&prevImg, &currImg, bb_tracker,bb_tracker,&scale);
-
-        //Extract subimage
-        float x,y,w,h;
-        x = floor(bb_tracker[0]+0.5);
-        y = floor(bb_tracker[1]+0.5);
-        w = floor(bb_tracker[2]-bb_tracker[0]+1+0.5);
-        h = floor(bb_tracker[3]-bb_tracker[1]+1+0.5);
-
-        //TODO: Introduce a check for a minimum size
-        if(!success || x < 0 || y < 0 || w <= 0 || h <= 0 || x +w > currMat.cols || y+h > currMat.rows || x!=x || y!=y || w!=w || h!=h) { //x!=x is check for nan
-            //Leave it empty
-        } else {
-            trackerBB.reset( new Rect(x,y,w,h) );
-        }
-    }
+}
 
 } /* namespace tld */
-
-
